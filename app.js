74b73f981363b29889eb76e47799ec9f34cf6c51
Kayla Daugherty <kayla.daugherty@gmail.com>
2021-03-24 18:15:52 -0700

    Updated name tag

---
+++ app.js
@@ -0,0 +1,8 @@
+const greetingInput = document.getElementById('greeting-input');
+const changeButton = document.getElementById('change-button');
+const greetingDisplay = document.getElementById('greeting-display');
+
+changeButton.addEventListener('click', () => {
+
+    greetingDisplay.textContent = greetingInput.value;
+});
